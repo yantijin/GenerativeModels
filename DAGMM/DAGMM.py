26e4e41d11ffdf5b9f3dcef48fc19f59ed2ffa2f
yantijin <yantijin@163.com>
2019-09-17 17:26:14 +0800

    0917

--- DAGMM/DAGMM.py
+++ DAGMM/DAGMM.py
@@ -1,19 +1,33 @@
 import tensorflow as tf
 from tensorflow import keras as ks
-from utils import *
+from .utils import *
 import numpy as np
 
 class DAGMM():
 
-    def __init__(self, seq_len, hidden_size, z_dim, estimate_hidden_size, n_gmm, lambda1=0.1, lambda2=0.005):
+    def __init__(self, seq_len, hidden_size, z_dim, estimate_hidden_size,
+                 n_gmm=4, lr=1e-4, batch_size=1024, max_epoch=200,
+                 lambda1=0.1, lambda2=0.005, data_path='./kdd_cup.npz', ):
         self.inputs = tf.placeholder(shape=[None, seq_len], dtype=tf.float32)
         self._seq_len = seq_len
         self._hidden_size = hidden_size
         self._z_dim = z_dim
         self._estimate_hidden_size = estimate_hidden_size
-        self._n_gmm = n_gmm
+        self._n_gmm = n_gmm # number of Gaussian components
         self._lambda1 = lambda1
         self._lambda2 = lambda2
+        self._data_path = data_path
+        self._batch_size = batch_size
+        self._max_epoch = max_epoch
+        self._lr = lr
+
+    @property
+    def batch_size(self):
+        return self._batch_size
+
+    @property
+    def max_epoch(self):
+        return self.max_epoch
 
     @property
     def seq_len(self):
@@ -42,6 +56,14 @@ class DAGMM():
     @property
     def lambda2(self):
         return self._lambda2
+
+    @property
+    def data_path(self):
+        return self._data_path
+
+    @property
+    def lr(self):
+        return self._lr
 
     def encoder(self):
         with tf.variable_scope('Encoder', reuse=tf.compat.v1.AUTO_REUSE):
@@ -145,12 +167,49 @@ class DAGMM():
         z = self.EstiInput()
         gamma = self.EstimateNet()
         EZ, lossSigma = self.EZ(gamma, z)
-        loss = tf.reduce_mean(tf.square(z, self.inputs)) + \
-               self.lambda1 * tf.reduce_mean(EZ) + \
+        reconError = tf.reduce_mean(tf.square(z, self.inputs))
+        EZError = tf.reduce_mean(EZ)
+        loss = reconError + \
+               self.lambda1 * EZError + \
                self.lambda2 * lossSigma
-        return loss
+        return loss, reconError, EZError, lossSigma
 
     def train(self, data):
+        train, test, train_labels, test_labels = data_loader(self.data_path)
+
+        # build tf graph
+        loss, reconError, EZError, lossSigma = self.loss()
+        train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(loss)
+
+        # train step
+        len_train = train.shape[0]
+        iterNum = len_train // self.batch_size
+        TrainIndex = list(range(len_train))
+        np.random.shuffle(TrainIndex)
+
+        testIterNum = test.shape[0] // self.batch_size
+        TestIndex = list(range(test.shape[0]))
+        np.random.shuffle(TestIndex)
+
+        with tf.Session() as sess:
+            for epoch in range(self.max_epoch):
+                for i in range(iterNum):
+                    inputData = train[TrainIndex[i*self.batch_size:(i+1)*self.batch_size]]
+                    _, loss_, recon_, EZ_, SIGMA_ = sess.run([train_op, loss, reconError, EZError, lossSigma],
+                                                             feed_dict={self.inputs: inputData})
+            if epoch % 5 == 0:
+                print("recon: {}, EZ: {}, SIGMA {} loss {}".format(recon_, EZ_, SIGMA_, loss_))
+
+            if epoch % 20 == 0:
+                for i in range(testIterNum):
+                    inputData = test[TestIndex[i*self.batch_size:(i+1)*self.batch_size]]
+                    _, loss_, recon_, EZ_, SIGMA_ = sess.run([train_op, loss, reconError, EZError, lossSigma],
+                                                             feed_dict={self.inputs: inputData})
+                print("Test loss:{}".format(loss_))
+
+
+
+
 
 
 
--- DAGMM/utils.py
+++ DAGMM/utils.py
@@ -8,7 +8,41 @@ def cosineSimilarity(labels, pred, axis):
     tmp = tf.multiply(pred, labels) / (labelNorm, predNorm)
     return 1-tmp
 
+
 def relative_euclidean_distance(laebls, pred, axis):
     t1 = tf.linalg.norm(laebls-pred, axis=axis)
     return t1 / tf.linalg.norm(laebls, axis=axis)
+
+
+def data_loader(data_path):
+    data = np.load(data_path)
+
+    labels = data['kdd'][:,-1]
+    features = data["kdd"][:,:-1]
+    N, D = features.shape
+
+    normal_data = features[labels==1]
+    normal_labels = labels[labels==1]
+
+    N_normal = normal_data.shape[0]
+
+    attack_data = features[labels==0]
+    attack_labels = labels[labels==0]
+
+    N_attack = attack_data.shape[0]
+
+    randIdx = np.arange(N_attack)
+    np.random.shuffle(randIdx)
+    N_train = N_attack // 2
+
+    train = attack_data[randIdx[:N_train]]
+    train_labels = attack_labels[randIdx[:N_train]]
+
+    test = attack_data[randIdx[N_train:]]
+    test_labels = attack_labels[randIdx[N_train:]]
+
+    test = np.concatenate((test,normal_data), axis=0)
+    test_labels = np.concatenate((test_labels, normal_labels), axis=0)
+
+    return train, test, train_labels, test_labels
 
