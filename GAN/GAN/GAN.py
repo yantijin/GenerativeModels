db1ebaf35e136829a94a12cbf90a54024953408e
yantijin <yantijin@163.com>
2019-10-08 18:05:38 +0800

    GAN1008

--- GAN/GAN/GAN.py
+++ GAN/GAN/GAN.py
@@ -2,6 +2,10 @@ import tensorflow as tf
 import numpy as np
 from .utils import *
 
+def sample_Z(batch_size, z_dim):
+    return np.random.normal(0., 1., size = [batch_size, z_dim])
+
+
 class GAN():
     def __init__(self, input_dim, hidden_dim, z_dim):
         self._input_dim = input_dim
@@ -23,6 +27,7 @@ class GAN():
         with tf.variable_scope('generator'):
             G1 = fcLayer(self._z, 'gen1layer', self.hidden_dim, activation_func=tf.nn.relu)
             G2 = fcLayer(G1, 'gen2layer', self.z_dim, activation_func=tf.nn.sigmoid)
+
             return G2
 
     def discriminator(self, input):
@@ -59,11 +64,39 @@ class GAN():
         x_test = test_data[:100, :]
         total_batch = int(train_size / batch_size)
 
+
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
+            index = 0
             # writer = tf.summary.FileWriter('./logs', sess.graph)
 
             for epoch in range(n_epochs):
                 np.random.shuffle(train_total_data)
                 train_data_ = train_total_data[:, :-10]
+
+                for batch in range(total_batch):
+                    index += 1
+
+                    with tf.Session() as sess:
+                        _, loss_D = sess.run([solver_D, Dloss],
+                                             feed_dict={
+                                                 self._input: train_data_[batch*batch_size:(batch+1)*batch_size],
+                                                 self._z: sample_Z(batch_size, z_dim=self.z_dim)
+                                             })
+                        _, loss_G = sess.run([solver_G, Gloss],
+                                             feed_dict={
+                                                 self._z: sample_Z(batch_size, self.z_dim)
+                                             })
+
+                    if index % 500 == 0:
+                        print("index:{}, lossD:{}, lossG:{}, ".format(index, loss_D, loss_G))
+                if epoch +1 == n_epochs:
+                    test_data_ = test_data[:100, :]
+
+
+
+
+
+
+
 
