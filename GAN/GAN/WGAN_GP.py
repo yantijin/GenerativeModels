5f1ff54dc1d07e77a767a5ac8b9ec0b9c46103cb
yantijin <yantijin@163.com>
2019-10-12 23:47:24 +0800

    WGAN_GPsucceed

--- GAN/GAN/WGAN_GP.py
+++ GAN/GAN/WGAN_GP.py
@@ -11,12 +11,13 @@ def sample_Z(batch_size, z_dim):
 
 
 class GAN():
-    def __init__(self, input_dim, hidden_dim, z_dim):
+    def __init__(self, input_dim, hidden_dim, z_dim, lamb):
         self._input_dim = input_dim
         self._hidden_dim = hidden_dim
         self._z_dim = z_dim
         self._input = tf.placeholder(tf.float32, [None, input_dim], name='input')
         self._z = tf.placeholder(tf.float32, [None, z_dim], name='noise_input')
+        self._lambda = lamb
 
     @property
     def input_dim(self):
@@ -38,44 +39,68 @@ class GAN():
     def z(self):
         return self._z
 
+    @property
+    def lamb(self):
+        return self._lambda
+
     def generator(self):
         with tf.variable_scope('generator', reuse=tf.compat.v1.AUTO_REUSE):
             G1 = fcLayer(self.z, 'gen1layer', self.hidden_dim, activation_func=tf.nn.relu)
             G2 = fcLayer(G1, 'gen2layer', self.input_dim, activation_func=tf.nn.sigmoid)
             GVarList = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'generator')
 
-        return G2, GVarList
+            return G2, GVarList
 
     def discriminator(self, input):
         with tf.variable_scope('discriminator', reuse=tf.compat.v1.AUTO_REUSE):
             G1 = fcLayer(input, 'dis1layer', self.hidden_dim, activation_func=tf.nn.relu)
-            G2 = fcLayer(G1, 'dis2layer', 1, activation_func=tf.nn.sigmoid)
+            G2 = fcLayer(G1, 'dis2layer', 1, activation_func=None)
             DVarList = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discriminator')
 
-        return G2, DVarList
+            return G2, DVarList
 
     def loss(self):
         # with tf.variable_scope('loss'):
         # generate fake data
+        """
+         与GAN相比，有四点改进
+        （1）Dis为判别器，此时为W——dis，应去掉sigmoid
+        （2）Dloss和Gloss中去掉对数
+        （3）为满足Lipschitz限制，将dis参数控制在一定范围内
+        （4）尽量不使用给予动量的优化算法，推荐RMSProp,SGD也可以
+        """
+
         x_fake, GVarList = self.generator()
+
+        epsilon = tf.random.uniform(tf.shape(x_fake), 0, 1)
+        x_hat = x_fake + epsilon*(self.input-x_fake)
 
         xreal_prob, DVarList = self.discriminator(self.input)
         xfake_prob, DVarList = self.discriminator(x_fake)
-        term1 = tf.reduce_mean(tf.log(xreal_prob + 1e-8))
-        term2 = tf.reduce_mean(tf.log(1 - xfake_prob + 1e-8))
-        Dloss = -(term1+term2)
+
+        # Dloss
+        term1 = tf.reduce_mean(xreal_prob)
+        term2 = tf.reduce_mean(xfake_prob)
+        gradient_term,_ = self.discriminator(x_hat)
+        gradients = tf.gradients(gradient_term, [x_hat])[0]
+        # print(gradients.get_shape().as_list())
+        GP = tf.reduce_mean(
+            (tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=-1) )
+            -1)**2)
+        Dloss = -(term1-term2-self.lamb*GP)
 
         # Gloss
-        Gloss = -tf.reduce_mean(tf.log(xfake_prob + 1e-8))
+        Gloss = -tf.reduce_mean(xfake_prob)
+
 
         return x_fake, Dloss, Gloss, GVarList, DVarList
 
 
-    def train(self, batch_size, learning_rate, n_epochs):
+    def train(self, batch_size, learning_rate, n_epochs,n_critic):
 
         xfake, Dloss, Gloss, GVarList, DVarList = self.loss()
-        solver_D = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(Dloss, var_list=DVarList)
-        solver_G = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(Gloss, var_list=GVarList)
+        solver_D = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0, beta2=0.9).minimize(Dloss, var_list=DVarList)
+        solver_G = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0, beta2=0.9).minimize(Gloss, var_list=GVarList)
 
         train_total_data, train_size, _, _, test_data, test_labels = prepare_MNIST_data()
         x_test = test_data[:100, :]
@@ -94,17 +119,20 @@ class GAN():
                 for batch in range(total_batch):
                     index += 1
 
-                    # with tf.Session() as sess:
+                    # 此处是采用5:1频率比进行优化的
+
                     noise = sample_Z(batch_size, z_dim=self._z_dim)
-                    _, loss_D = sess.run([solver_D, Dloss],
-                                         feed_dict={
-                                             self._input: train_data_[batch*batch_size:(batch+1)*batch_size],
-                                             self._z: noise
-                                         })
+                    for ind in range(n_critic):
+                        _, loss_D = sess.run([solver_D, Dloss],
+                                               feed_dict={
+                                                 self._input: train_data_[batch*batch_size:(batch+1)*batch_size],
+                                                 self._z: noise
+                                               })
                     _, loss_G = sess.run([solver_G, Gloss],
                                          feed_dict={
                                              self._z: sample_Z(batch_size, self.z_dim)
                                          })
+
 
                     if index % 500 == 0:
                         print("index:{}, lossD:{}, lossG:{}, ".format(index, loss_D, loss_G))
@@ -121,18 +149,20 @@ class GAN():
                         j = int(idx / int(batch_size / 4))
                         image_ = imresize(image, size=(28, 28), interp='bicubic')
                         img[j * 28: j * 28 + 28, i * 28: i * 28 + 28] = image_
-                    imsave('./result' + str(epoch) + '.jpg', img)
+                    imsave('./WGAN_GP' + str(epoch) + '.jpg', img)
 
 
 if __name__ == '__main__':
     tf.reset_default_graph()
     batch_size = 100
-    learning_rate = 1e-3
+    learning_rate = 1e-4
     n_epochs = 100
-    gan = GAN(input_dim=28*28, hidden_dim=128, z_dim=100)
-    gan.train(batch_size, learning_rate, n_epochs)
+    n_critic=5
+    lamb = 10
 
+    gan = GAN(input_dim=28*28, hidden_dim=128, z_dim=100, lamb=lamb)
 
+    gan.train(batch_size, learning_rate, n_epochs, n_critic)
 
 
 
